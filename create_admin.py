8e8c90c1ee3d48743bf436534ae2441414bc72ff
Yomarakesha <yomarakesha@gmail.com>
2025-08-27 18:58:34 +0500

    without db

--- create_admin.py
+++ create_admin.py
@@ -4,9 +4,9 @@ from werkzeug.security import generate_password_hash
 
 with app.app_context():
     if not User.query.filter_by(username='admin').first():
-        admin_user = User(username='admin', email='admin@example.com', password=generate_password_hash('admin'))
+        admin_user = User(username='admin', email='admin@example.com', password='admin')
         db.session.add(admin_user)
         db.session.commit()
-        print("✅ Админ успешно создан: admin / admin")
+        print("Admin user created: admin / admin (plain password)")
     else:
-        print("⚠️ Админ уже существует")
+        print("Admin already exists")
